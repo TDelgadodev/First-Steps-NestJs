b4c489b4ae1de5533ff3a09610c46b9fb67a84c7
Thiago <thiagodelgado.dev@gmail.com>
2023-12-01 18:42:39 -0300

    add validation on update method

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,31 @@
-import { TaskStatus } from "../task.entity"
+import { TaskStatus } from "../task.entity";
+import { IsString, IsNotEmpty, MinLength, MaxLength, IsOptional, IsIn } from "class-validator"
 
-export class CreateTaskDto {
+export class CreateTaskDto {   
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3) 
+    @MaxLength(16)
     title: string
+
+    @IsNotEmpty()
+    @IsString()
     description: string
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title?: string
+
+    @IsString()
+    @IsOptional()
+    @MinLength(6) 
+    @MaxLength(120)
     description?: string
+ 
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.PENDING])
     status?: TaskStatus
 }
