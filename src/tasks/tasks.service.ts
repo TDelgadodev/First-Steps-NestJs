86f9763b0c87d0409facb83cb3f7e58907dfd7cd
Thiago <thiagodelgado.dev@gmail.com>
2023-12-01 13:03:44 -0300

    list and create method on services & controllers

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'First Task',
+      description: 'Some description',
+      status: TaskStatus.PENDING
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+  createTask(title: string, description: string) {
+    const task = {
+        id: v4(),
+        title: title,
+        description: description,
+        status: TaskStatus.PENDING
+    }
+
+    this.tasks.push(task);
+    return task
+  }
+  updateTask() {}
+  deleteTask() {}
+}
